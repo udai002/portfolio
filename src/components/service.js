dd1ac51ee2dce00ce799f3bb546c0f434d283ab9
udai002 <94735980+udai002@users.noreply.github.com>
2023-01-11 19:47:27 +0530

    edit from github

--- src/components/service.js
+++ src/components/service.js
@@ -8,7 +8,7 @@ const service = () => {
       <div className="flex flex-row flex-wrap">
         <div className="text-center flex flex-col justify-around w-[300px] h-[250px] shadow-md m-4 bg-white hover:bg-red-500 duration-75 hover:text-white">
           <div className='flex justify-center'><MdWebAsset className='text-3xl text-red-600'/></div>
-          <h1 className='font-bold text-xl '>Web development</h1>
+          <h1 className='font-bold text-xl '>Web development edtied in github</h1>
           <p className='font-serif text-lg'>Developing a web Application like Ecommerce website , blogs , bussiness websites and Education websites using PHP , Reactjs</p>
         </div>
 
